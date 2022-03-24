267d8a4b221198d629427a3d234bf328c383bb52
Marty Chen <marty_chen@mail.gss.com.tw>
2022-03-24 09:37:45 +0800

    取得 NBA 台灣 RSS 圖片

--- src/news/udn_nba.ts
+++ src/news/udn_nba.ts
@@ -17,12 +17,14 @@ export class NBATaiwanNewsCrawler {
             .map((_, item) => {
                 let title = $(item).find('h3').text();
                 let link = rootUrl + $(item).find('a').attr('href');
+                let image = $(item).find('img').attr('data-src');
                 let content = $(item).find('p').text();
                 let pubDate = $(item).find('b').text();
-                
+
                 return {
                     title,
                     link,
+                    image,
                     content,
                     pubDate,
                 };
