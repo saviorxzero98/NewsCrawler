ffe0b3bbe49b05fa8e59dc1eea9dbd66c5fbaa54
saviorxzero98 <saviorxzero98@gmail.com>
2022-04-07 22:35:03 +0800

    加入 UDN RSS

--- src/news/udn/router.ts
+++ src/news/udn/router.ts
@@ -2,17 +2,98 @@ import { FeedBuilder } from '../../feeds/feedBuilder';
 import { NBATaiwanNewsCrawler } from './nba_tw';
 import { WorldJournalNewsCrawler } from './worldjournal';
 import { ServiceContext } from '../../services/service';
+import { UDNNewsCrawler } from './udn';
 
 
 const path = {
-    udn: 'udn',
+    udn: 'udn/news',
+    udnStar: 'udn/star',
+    udnHealth: 'udn/health',
+    udnGlobal: 'udn/global',
+    udnGame: 'udn/game',
+    udnOpinion: 'udn/opinion',
     nba: 'nba-tw',
     worldjournal: 'worldjournal'
 }
 
 export class UDNNewsRouter {
     public static router(services: ServiceContext) {
+        // 聯合新聞網
+        services.app.get(`/${path.udn}/:category?/:subcategory?`, async (req, res) => {
+            let category = req.params.category ?? 'latest';
+            let subcategory = req.params.subcategory ?? '';
+            let limit = Number(req.query.limit ?? services.config.maxRssCount);
+            let opencc = String(req.query.opencc ?? '');
 
+            let crawler = new UDNNewsCrawler(services);
+            let data = await crawler.getNews(category, subcategory, limit);
+            let feedBuilder = new FeedBuilder(data.title, data.link).setOpenCC(opencc);
+            feedBuilder = feedBuilder.addItems(data.items);
+            res.send(feedBuilder.create());
+        });
+
+        services.app.get(`/${path.udnStar}/:category?`, async (req, res) => {
+            let category = req.params.category ?? '10088';
+            let limit = Number(req.query.limit ?? services.config.maxRssCount);
+            let opencc = String(req.query.opencc ?? '');
+
+            let crawler = new UDNNewsCrawler(services);
+            let data = await crawler.getStarNews(category, limit);
+            let feedBuilder = new FeedBuilder(data.title, data.link).setOpenCC(opencc);
+            feedBuilder = feedBuilder.addItems(data.items);
+            res.send(feedBuilder.create());
+        });
+
+        services.app.get(`/${path.udnHealth}/:category?/:subcategory?`, async (req, res) => {
+            let category = req.params.category ?? '5681';
+            let subcategory = req.params.subcategory ?? '';
+            let limit = Number(req.query.limit ?? services.config.maxRssCount);
+            let opencc = String(req.query.opencc ?? '');
+
+            let crawler = new UDNNewsCrawler(services);
+            let data = await crawler.getHealthNews(category, subcategory, limit);
+            let feedBuilder = new FeedBuilder(data.title, data.link).setOpenCC(opencc);
+            feedBuilder = feedBuilder.addItems(data.items);
+            res.send(feedBuilder.create());
+        });
+
+        services.app.get(`/${path.udnGlobal}/:category?`, async (req, res) => {
+            let category = req.params.category ?? '8662';
+            let limit = Number(req.query.limit ?? services.config.maxRssCount);
+            let opencc = String(req.query.opencc ?? '');
+
+            let crawler = new UDNNewsCrawler(services);
+            let data = await crawler.getGlobalNews(category, limit);
+            let feedBuilder = new FeedBuilder(data.title, data.link).setOpenCC(opencc);
+            feedBuilder = feedBuilder.addItems(data.items);
+            res.send(feedBuilder.create());
+        });
+
+        services.app.get(`/${path.udnGame}/:category?`, async (req, res) => {
+            let category = req.params.category ?? '';
+            let limit = Number(req.query.limit ?? services.config.maxRssCount);
+            let opencc = String(req.query.opencc ?? '');
+
+            let crawler = new UDNNewsCrawler(services);
+            let data = await crawler.getGameNews(category, limit);
+            let feedBuilder = new FeedBuilder(data.title, data.link).setOpenCC(opencc);
+            feedBuilder = feedBuilder.addItems(data.items);
+            res.send(feedBuilder.create());
+        });
+
+        services.app.get(`/${path.udnOpinion}/:category?`, async (req, res) => {
+            let category = req.params.category ?? '';
+            let limit = Number(req.query.limit ?? services.config.maxRssCount);
+            let opencc = String(req.query.opencc ?? '');
+
+            let crawler = new UDNNewsCrawler(services);
+            let data = await crawler.getOpinionNews(category, limit);
+            let feedBuilder = new FeedBuilder(data.title, data.link).setOpenCC(opencc);
+            feedBuilder = feedBuilder.addItems(data.items);
+            res.send(feedBuilder.create());
+        });
+
+        // NBA 台灣
         services.app.get(`/${path.nba}/:category?`, async (req, res) => {
             let category = req.params.category ?? 'newest';
             let limit = Number(req.query.limit ?? services.config.maxRssCount);
@@ -25,6 +106,7 @@ export class UDNNewsRouter {
             res.send(feedBuilder.create());
         });
 
+        // 世界新聞網
         services.app.get(`/${path.worldjournal}/:category?/:language?`, async (req, res) => {
             let category = req.params.category ?? 'newest';
             let language = req.params.language ?? '';
---
+++ src/news/udn/udn.ts
@@ -0,0 +1,212 @@
+import * as moment from 'moment';
+
+import { NewsCrawler } from '../newsCrawler';
+import { ServiceContext } from '../../services/service';
+
+const rootUrl = 'https://udn.com';
+const starRootUrl = 'https://stars.udn.com';
+const healthRootUrl = 'https://health.udn.com';
+const globalRootUrl = 'https://global.udn.com';
+const gameRootUrl = 'https://game.udn.com';
+const opinionRootUrl = 'https://opinion.udn.com/';
+
+const title = '聯合新聞網';
+const starTitle = 'UDN 噓！星聞';
+const healthTitle = 'UDN 元氣網';
+const globalTitle = 'UDN 轉角國際';
+const gameTitle = 'UDN 遊戲角落';
+const opinionTitle = 'UDN 名人堂';
+
+const newsRssMap = {
+    'latest': '最新',
+    'hottest': '熱門',
+    '6638': '要聞',
+    '6640': '兩岸',
+    '6641': '地方',
+    '6643': '評論',
+    '6644': '財經',
+    '6645': '股市',
+    '6656': '政治',
+    '6639': '社會',
+    '6649': '生活',
+    '6999': 'MLB',
+    '7002': 'NBA',
+    '7225': '國際',
+    '7226': '數位',
+    '7227': '運動',
+    '7240': '產業',
+    '7314': '綜合',
+    '10930': '軍事',
+    '120909': 'Oops',
+    '120940': '疫情'
+};
+
+const starNewsRssMap = {
+    '10088': '熱門星聞',
+    '10089': '藝人',
+    '10090': '電影',
+    '10091': '電視',
+    '10092': '音樂',
+    '120661': '熱搜',
+    '10093': '娛樂有評',
+}
+
+const healthNewRssMap = {
+    '5681': '最新',
+    '120949': '新冠肺炎',
+    '5680': '該看哪科',
+    '5683': '癌症',
+    '5684': '養生',
+    '5686': '性愛',
+    '7391': '名人',
+    '7732': '活動',
+    '10691': '失智',
+    '122318': '圖表看健康',
+    '122416': '醫聲'
+}
+
+export class UDNNewsCrawler extends NewsCrawler {
+    constructor(services: ServiceContext) {
+        super(services);
+    }
+
+    public async getNews(category: string = 'latest', subCategory = '', count: number = 15) {
+        let url = '';
+        let categoryName = '';
+        if (category) {
+            if (category === 'latest' || category === 'hottest') {
+                url = `${rootUrl}/rssfeed/${category}`;
+                categoryName = newsRssMap[category];
+            }
+            else if (/^\d+$/.test(category)) {
+                url =  `${rootUrl}/rssfeed/news/2/${category}`;
+                categoryName = newsRssMap[category] || '';
+
+                if (subCategory && /^\d+$/.test(subCategory)) {
+                    url = `${url}/${subCategory}`;
+
+                    if (newsRssMap[subCategory]) {
+                        categoryName = newsRssMap[subCategory];
+                    }
+                }
+            }
+        }
+
+        let list = await this.getRSSNewsList({
+            url,
+            count
+        });
+
+        return {
+            title: `${title} ${categoryName}`,
+            link: url,
+            items: list
+        };
+    }
+
+    public async getStarNews(category: string = '10088', count: number = 15) {
+        let url = '';
+        let categoryName = '';
+        if (category) {
+            if (/^\d+$/.test(category)) {
+                url =  `${starRootUrl}/rss/news/1022/${category}`;
+                categoryName = starNewsRssMap[category] || '';
+            }
+        }
+
+        let list = await this.getRSSNewsList({
+            url,
+            count
+        });
+
+        return {
+            title: `${starTitle} ${categoryName}`,
+            link: url,
+            items: list
+        };
+    }
+
+    public async getHealthNews(category: string = '5681', subCategory = '', count: number = 15) {
+        let url = '';
+        let categoryName = '';
+        if (category && /^\d+$/.test(category)) {
+            url =  `${healthRootUrl}/rss/news/1005/${category}`;
+                categoryName = healthNewRssMap[category] || '';
+
+                if (subCategory && /^\d+$/.test(subCategory)) {
+                    url = `${url}/${subCategory}`;
+                }
+        }
+
+        let list = await this.getRSSNewsList({
+            url,
+            count
+        });
+
+        return {
+            title: `${healthTitle} ${categoryName}`,
+            link: url,
+            items: list
+        };
+    }
+
+    public async getGlobalNews(category: string = '8662', count: number = 15) {
+        let url = '';
+        if (category) {
+            if (/^\d+$/.test(category)) {
+                url =  `${globalRootUrl}/rss/news/1020/${category}`;
+            }
+        }
+
+        let list = await this.getRSSNewsList({
+            url,
+            count
+        });
+
+        return {
+            title: `${globalTitle}`,
+            link: url,
+            items: list
+        };
+    }
+
+    public async getGameNews(category: string = '', count: number = 15) {
+        let url = `${gameRootUrl}/rss/news/2003`;
+        if (category) {
+            if (/^\d+$/.test(category)) {
+                url =  `${gameRootUrl}/rss/news/2003/${category}`;
+            }
+        }
+
+        let list = await this.getRSSNewsList({
+            url,
+            count
+        });
+
+        return {
+            title: `${gameTitle}`,
+            link: url,
+            items: list
+        };
+    }
+
+    public async getOpinionNews(category: string = '', count: number = 15) {
+        let url = `${opinionRootUrl}/rss/news/1008`;
+        if (category) {
+            if (/^\d+$/.test(category)) {
+                url =  `${opinionRootUrl}/rss/news/1008/${category}`;
+            }
+        }
+
+        let list = await this.getRSSNewsList({
+            url,
+            count
+        });
+
+        return {
+            title: `${opinionTitle}`,
+            link: url,
+            items: list
+        };
+    }
+}
