a9f8caafcbdccbe774e78b658222bd3632cb89b6
Marty Chen <marty_chen@mail.gss.com.tw>
2022-04-07 10:51:26 +0800

    加入 4Gamers RSS

---
+++ src/news/acg/4gamer.ts
@@ -0,0 +1,152 @@
+import * as axios from 'axios';
+import * as moment from 'moment';
+
+import { NewsCrawler } from '../newsCrawler';
+import { ServiceContext } from '../../services/service';
+import * as utils from '../../feeds/utils';
+
+const rootUrl = 'https://www.4gamers.com.tw';
+const title = '4Gamers';
+
+const httpClient = axios.default;
+
+const categoryMap = {
+    '352': '遊戲資訊',
+    '353': '電子競技',
+    '355': '科技硬體',
+    '359': '深度專題',
+    '1109': '實況直播',
+    '1115': '品牌提供',
+    '1116': '影劇動漫',
+    '1117': '潮流宅物',
+    '1118': '限時免費',
+    '1119': '🔞成人限定',
+    '1120': '👬男上加男'
+}
+
+
+export class FourGamersNewsCrawler extends NewsCrawler {
+    constructor(services: ServiceContext) {
+        super(services);
+    }
+
+    public async getNewsByCategory(category: string = '352', count: number = 15) {
+        if (category && categoryMap[category]) {
+            let url = `${rootUrl}/site/api/news/by-category/${category}?pageSize=${count}`;
+
+            let response = await httpClient.get(url);
+            let results = response.data.data.results;
+            let list = results.map((item) => ({
+                title: item.title,
+                image: item.smallBannerUrl,
+                description: '',
+                pubDate: new Date(item.createPublishedAt * 1),
+                link: item.canonicalUrl,
+            }))
+
+            let items = await this.getNewsDetials({
+                list,
+                options: utils.crawlerOptions,
+                callback: (item, content) => {
+                    let description = content('meta[name="description"]').attr('content');
+                    let image = content('meta[property="og:image"]').attr('content');
+                    item.description = description;
+                    item.image = image || item.image;
+                    return item;
+                }
+            });
+            
+            return {
+                title: `${title} ${categoryMap[category]}`,
+                link: url,
+                items: items
+            };
+        }
+
+        return {
+            title: `${title}`,
+            link: rootUrl,
+            items: []
+        };
+    }
+
+    public async getNewsByTag(tag: string = '', count: number = 15) {
+        if (tag) {
+            let url = `${rootUrl}/site/api/news/by-tag?tag=${encodeURIComponent(tag)}&pageSize=${count}`;
+
+            let response = await httpClient.get(url);
+            let results = response.data.data.results;
+            let list = results.map((item) => ({
+                title: item.title,
+                image: item.smallBannerUrl,
+                description: '',
+                pubDate: new Date(item.createPublishedAt * 1),
+                link: item.canonicalUrl,
+            }))
+
+            let items = await this.getNewsDetials({
+                list,
+                options: utils.crawlerOptions,
+                callback: (item, content) => {
+                    let description = content('meta[name="description"]').attr('content');
+                    let image = content('meta[property="og:image"]').attr('content');
+                    item.description = description;
+                    item.image = image || item.image;
+                    return item;
+                }
+            });
+            
+            return {
+                title: `${title} ${tag}`,
+                link: url,
+                items: items
+            };
+        }
+
+        return {
+            title: `${title} ${tag}`,
+            link: rootUrl,
+            items: []
+        };
+    }
+
+    public async getNewsByTopic(topic: string = '', count: number = 15) {
+        if (topic) {
+            let url = `${rootUrl}/site/api/news/option-cfg/${topic}?pageSize=${count}`;
+
+            let response = await httpClient.get(url);
+            let results = response.data.data.results;
+            let list = results.map((item) => ({
+                title: item.title,
+                image: item.smallBannerUrl,
+                description: '',
+                pubDate: new Date(item.createPublishedAt * 1),
+                link: item.canonicalUrl,
+            }))
+
+            let items = await this.getNewsDetials({
+                list,
+                options: utils.crawlerOptions,
+                callback: (item, content) => {
+                    let description = content('meta[name="description"]').attr('content');
+                    let image = content('meta[property="og:image"]').attr('content');
+                    item.description = description;
+                    item.image = image || item.image;
+                    return item;
+                }
+            });
+            
+            return {
+                title: `${title} ${topic}`,
+                link: url,
+                items: items
+            };
+        }
+
+        return {
+            title: `${title} ${topic}`,
+            link: rootUrl,
+            items: []
+        };
+    }
+}
--- src/news/acg/router.ts
+++ src/news/acg/router.ts
@@ -1,13 +1,16 @@
 import { FeedBuilder } from '../../feeds/feedBuilder';
 import { ServiceContext } from '../../services/service';
 
+import { FourGamersNewsCrawler } from './4gamer';
 import { GamerGNNNewsCrawler } from './gamer_gnn';
 
 const path = {
     gamerGnn: 'gamer/gnn',
+    fourGamers: '4gamers'
 }
 export class ACGNewsRouter {
     public static router(services: ServiceContext) {
+        // 巴哈姆特 GNN
         services.app.get(`/${path.gamerGnn}/:category?`, async (req, res) => {
             let category = req.params.category ?? '';
             let limit = Number(req.query.limit ?? services.config.maxRssCount);
@@ -15,6 +18,44 @@ export class ACGNewsRouter {
             
             let crawler = new GamerGNNNewsCrawler(services);
             let data = await crawler.getNews(category, limit);
+            let feedBuilder = new FeedBuilder(data.title, data.link).setOpenCC(opencc);
+            feedBuilder = feedBuilder.addItems(data.items);
+            res.send(feedBuilder.create());
+        });
+
+
+        // 4Gamers
+        services.app.get(`/${path.fourGamers}/category/:category?`, async (req, res) => {
+            let category = req.params.category ?? '352';
+            let limit = Number(req.query.limit ?? services.config.maxRssCount);
+            let opencc = String(req.query.opencc ?? '');
+            
+            let crawler = new FourGamersNewsCrawler(services);
+            let data = await crawler.getNewsByCategory(category, limit);
+            let feedBuilder = new FeedBuilder(data.title, data.link).setOpenCC(opencc);
+            feedBuilder = feedBuilder.addItems(data.items);
+            res.send(feedBuilder.create());
+        });
+        // 4Gamers
+        services.app.get(`/${path.fourGamers}/tag/:tag?`, async (req, res) => {
+            let tag = req.params.tag ?? '';
+            let limit = Number(req.query.limit ?? services.config.maxRssCount);
+            let opencc = String(req.query.opencc ?? '');
+            
+            let crawler = new FourGamersNewsCrawler(services);
+            let data = await crawler.getNewsByTag(tag, limit);
+            let feedBuilder = new FeedBuilder(data.title, data.link).setOpenCC(opencc);
+            feedBuilder = feedBuilder.addItems(data.items);
+            res.send(feedBuilder.create());
+        });
+        // 4Gamers
+        services.app.get(`/${path.fourGamers}/topic/:topic?`, async (req, res) => {
+            let topic = req.params.topic ?? '';
+            let limit = Number(req.query.limit ?? services.config.maxRssCount);
+            let opencc = String(req.query.opencc ?? '');
+            
+            let crawler = new FourGamersNewsCrawler(services);
+            let data = await crawler.getNewsByTopic(topic, limit);
             let feedBuilder = new FeedBuilder(data.title, data.link).setOpenCC(opencc);
             feedBuilder = feedBuilder.addItems(data.items);
             res.send(feedBuilder.create());
