bb275c8dba9c6d6b189a31d7d3c6ef187e1a07b5
saviorxzero98 <saviorxzero98@gmail.com>
2022-04-09 20:06:18 +0800

    修復 中時電子報

--- src/news/chinatimes/chinatimes.ts
+++ src/news/chinatimes/chinatimes.ts
@@ -8,23 +8,23 @@ const rootUrl = 'https://www.chinatimes.com';
 const title = '中時電子報';
 
 const categoryMap = {
-    realtimenews: '即時',
-    politic: '政治',
-    opinion: '言論',
-    life: '生活',
-    star: '娛樂',
-    money: '財經',
-    world: '國際',
-    chinese: '兩岸',
-    society: '社會',
-    armament: '軍事',
-    technologynews: '科技',
-    sports: '體育',
-    hottopic: '網推',
-    tube: '有影',
-    health: '健康',
-    fortune: '運勢',
-    taiwan: '寶島'
+    'realtimenews': '即時',
+    'politic': '政治',
+    'opinion': '言論',
+    'life': '生活',
+    'star': '娛樂',
+    'money': '財經',
+    'world': '國際',
+    'chinese': '兩岸',
+    'society': '社會',
+    'armament': '軍事',
+    'sports': '體育',
+    'hottopic': '網推',
+    'tube': '有影',
+    'health': '健康',
+    'fortune': '運勢',
+    'taiwan': '寶島',
+    'technologynews': '科技',
 };
 
 export class ChinaTimesNewsCrawler extends NewsCrawler {
@@ -32,9 +32,20 @@ export class ChinaTimesNewsCrawler extends NewsCrawler {
         super(services);
     }
     
-    public async getNews(category: string = 'realtimenews', count: number = 15) {
-        let url = `${rootUrl}/${category}/?chdtv`;
-        
+    public async getNews(category: string = 'realtimenews', subcategory: string = '', count: number = 15) {
+        let url = `${rootUrl}/realtimenews/?chdtv`;
+        let categoryName = '';
+        if (category) {
+            categoryName = categoryMap[category];
+
+            if (subcategory) {
+                url = `${rootUrl}/${category}/${subcategory}/?chdtv`;
+            }
+            else {
+                url = `${rootUrl}/${category}/?chdtv`; 
+            }
+        }
+
         let crawler = {
             selector: 'section.article-list ul div.row',
             callback: ($, i) => {
@@ -43,13 +54,12 @@ export class ChinaTimesNewsCrawler extends NewsCrawler {
                 let description = $(i).find('p.intro').text();
                 let image = $(i).find('img.photo').attr('src') || '';
                 let pubDate = $(i).find('time').attr('datetime');
-
                 return {
                     title,
                     link,
                     description,
                     image,
-                    date: moment(pubDate, 'HH:mm YYYY/MM/DD').toDate(),
+                    date: moment(pubDate, 'YYYY-MM-DD HH:mm').toDate(),
                 };
             }
         };
@@ -59,9 +69,11 @@ export class ChinaTimesNewsCrawler extends NewsCrawler {
             count,
             crawlers: [ crawler ]
         })
-     
+        
+        
+
         return {
-            title: `${title} ${categoryMap[category]}`,
+            title: `${title} ${categoryName}`,
             link: url,
             items: list,
         };
